52acb42f73145524e320d9425dd06a3bf5659ab8
Dominik Walnsch <d.walnsch@outlook.de>
2018-02-12 15:28:42 +0100

    % fixed quirks on first opening xstate tab

--- src/register.js
+++ src/register.js
@@ -6,16 +6,9 @@ import { render } from "./cyto";
 import { build } from "./graphBuilder/statechart";
 
 const styles = {
-  // cy: {
-  //   display: "flex",
-  //   flexDirection: "row",
-  //   alignItems: "stretch",
-  //   height: "100vh",
-  //   width: "100vw"
-  // }
   cy: {
-    width: "100%",
-    height: "100%"
+    height: "100vh",
+    width: "100vw"
   }
 };
 
@@ -23,25 +16,34 @@ class XStateGraph extends React.Component {
   constructor(...args) {
     super(...args);
     this.buildGraph = this.buildGraph.bind(this);
-    this.curMachine = "";
+    this.machine = { id: "" };
   }
 
-  buildGraph({ machine, currentState }) {
-    if (this.curMachine !== machine.id) {
-      this.curMachine = machine.id;
-      this.graph = render(
-        this.cNode,
-        build(machine.states, machine.initial, null, currentState),
-        event => {
-          const { channel } = this.props;
-          channel.emit("xstate/transition", event);
-        }
-      );
-    } else {
-      this.graph.setState(currentState);
+  buildGraph({ machine, currentState, forceRender = false }) {
+    if (this.cNode && machine.id) {
+      if (this.machine.id !== machine.id || forceRender) {
+        this.machine = machine;
+        this.currentState = currentState;
+        this.graph = render(
+          this.cNode,
+          build(machine.states, machine.initial, null, currentState),
+          event => {
+            const { channel } = this.props;
+            channel.emit("xstate/transition", event);
+          }
+        );
+      } else {
+        this.graph.setState(currentState);
+      }
     }
   }
-
+  componentDidUpdate() {
+    this.buildGraph({
+      machine: this.machine,
+      currentState: this.currentState,
+      forceRender: true
+    });
+  }
   componentDidMount() {
     const { channel, api } = this.props;
     channel.on("xstate/buildGraph", this.buildGraph);
